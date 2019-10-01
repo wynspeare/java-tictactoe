3c533a4bc0e8ba26e91c1499649f75506eb1b383
wynspeare <caroline@8thlight.com>
2019-10-01 18:17:27 -0500

    HTML page sent to server and displayed

--- src/main/java/application/TTT.java
+++ src/main/java/application/TTT.java
@@ -1,5 +1,7 @@
 package application;
 
+import application.handlers.ResourceHandler;
+import application.handlers.StaticHandler;
 import server.HTTPServer;
 import server.Router;
 import server.handlers.DefaultHandler;
@@ -22,6 +24,12 @@ public class TTT {
   public static Router createRouter() {
     Router router = new Router(serverLogger);
     router.addRoute("GET", "/", new DefaultHandler());
+    router.addRoute("GET", "/ttt", new ResourceHandler("assets/public/index.html"));
+    router.addRoute("GET", "/styles.css", new StaticHandler());
+    router.addRoute("GET", "/favicon.ico", new StaticHandler());
+    router.addRoute("GET", "/ttt.png", new StaticHandler());
+
+
     return router;
   }
 }
---
+++ start-ttt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+./gradlew build
+./gradlew jar
+java -cp tictactoe-1.0-SNAPSHOT.jar:build/libs/*:. application.TTT & MyPID=$!
