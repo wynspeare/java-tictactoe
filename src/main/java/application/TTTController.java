eaec5a16b88c4a231619c5536ffb6c7a1e47de90
wynspeare <caroline@8thlight.com>
2019-10-03 16:26:18 -0500

    Added html/css for board and created Controller and UnknownResourceHandler

---
+++ src/main/java/application/TTTController.java
@@ -0,0 +1,40 @@
+package server;
+
+import HTTPcomponents.Methods;
+import application.handlers.UnknownResourceHandler;
+import server.handlers.IHandler;
+import server.request.Request;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TTTController implements IController {
+
+  public Response handle(Request request, HashMap<String, List<HashMap<Methods, IHandler>>> routes) {
+    Response response;
+    String requestPath = request.getRequestPath();
+    if (isValidURI(requestPath, routes)) {
+      List<HashMap<Methods, IHandler>> allowedMethods = routes.get(requestPath);
+      IHandler handler = getAllowedMethodHandler(allowedMethods, request);
+      response = handler.buildResponse(request);
+    } else {
+      IHandler handler = new UnknownResourceHandler();
+      response = handler.buildResponse(request);
+    }
+    return response;
+  }
+
+  public IHandler getAllowedMethodHandler(List<HashMap<Methods, IHandler>> allowedMethods, Request request) {
+    IHandler handler = null;
+    for (HashMap<Methods, IHandler> element : allowedMethods) {
+      if (element.containsKey(request.getMethod())) {
+        handler = element.get(request.getMethod());
+      }
+    }
+    return handler;
+  }
+
+  public boolean isValidURI(String uri, HashMap<String, List<HashMap<Methods, IHandler>>> routes) {
+    return routes.containsKey(uri);
+  }
+}
--- src/test/java/application/TTTTest.java
+++ src/test/java/application/TTTTest.java
@@ -1,7 +1,9 @@
 package application;
 
 import org.junit.Test;
+import server.IController;
 import server.Router;
+import server.TTTController;
 import server.logger.LoggerSpy;
 
 import static org.junit.Assert.*;
@@ -10,7 +12,8 @@ public class TTTTest {
   @Test
   public void importedServerJARCanBeTested() {
     LoggerSpy testLogger = new LoggerSpy();
-    Router router = new Router(testLogger);
+    IController controller = new TTTController();
+    Router router = new Router(testLogger, controller);
 
     assertTrue(router.routes.isEmpty());
   }
