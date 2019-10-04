d2211fd11e53b2ab6ec98ae3f3aae65317f793f9
wynspeare <caroline@8thlight.com>
2019-10-04 11:12:44 -0500

    FE board listens for clicks and marks board. Added filled space validation.

--- assets/public/script.js
+++ assets/public/script.js
@@ -18,13 +18,59 @@ function readTextFile(file, callback) {
 }
 
 const cells = document.querySelectorAll('.cell-contents')
+const startGame = document.querySelector('.btn')
+let modal = document.getElementById("myModal");
+let closeModal = document.getElementsByClassName("close")[0];
+let boardContainer = document.getElementsByClassName("board")[0];
 
 readTextFile("/board.json", function(text){
-    var data = JSON.parse(text);
+    let data = JSON.parse(text);
     console.log(data.board)
-    const board = data.board
+    let board = data.board
 
     cells.forEach(function (cell, index) {
         cell.innerHTML = board[index];
     })
+
+    function clearCells(event) {
+        cells.forEach(cell => {
+            cell.innerHTML = " ";
+        })
+    }
+
+    function markCell(event) {
+        let cell = document.querySelector(`[id="${event.target.id}"]`)
+
+        console.log(isCellFilled(cell.innerHTML))
+        if (!isCellFilled(cell.innerHTML)) {
+            cell.innerHTML = "X";
+            let cellIndex = parseInt(event.target.id);
+            board[cellIndex - 1] = "X";
+            console.log(board);
+        // updateJsonBoard(board)
+        } else {
+            modal.style.display = "block";
+        }
+    }
+
+    function updateJsonBoard(board) {
+        // trigger a POST request with the updatedBoard
+    }
+
+    function isCellFilled(cell) {
+       return (cell == "X" || cell == "O" ? true : false)
+    }
+
+    closeModal.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+    startGame.addEventListener('click', clearCells)
+    boardContainer.addEventListener('click', markCell)
 });
