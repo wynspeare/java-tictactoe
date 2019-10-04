4a3b4b9b71b29790a144da6b7bf24ea4d709932f
wynspeare <caroline@8thlight.com>
2019-10-04 11:12:44 -0500

    FE board listens for clicks and marks board. Added filled space validation.

--- start-ttt.sh
+++ start-ttt.sh
@@ -2,4 +2,4 @@
 set -ex
 ./gradlew build
 ./gradlew jar
-java -cp tictactoe-1.0-SNAPSHOT.jar:build/libs/*:. application.TTT & MyPID=$!
+java -cp tictactoe-1.0-SNAPSHOT.jar:build/libs/*:. application.TTT & echo $!
